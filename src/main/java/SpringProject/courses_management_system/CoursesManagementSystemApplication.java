74b2d1ab0664d6eca09273fc3566af881bec1a29
MennaaWaleed <meenna154@gmail.com>
2026-07-24 21:51:14 +0300

    Initial project setup

---
+++ src/main/java/SpringProject/courses_management_system/CoursesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package SpringProject.courses_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManagementSystemApplication.class, args);
+	}
+
+}
