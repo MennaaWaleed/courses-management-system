902cfbb390edbfc7ba7f056552bedf9c2e10c913
Mostafa AbdElhakim <130708145+mostafahakim03@users.noreply.github.com>
2026-07-27 11:02:26 +0300

    Initial Security

---
+++ src/main/java/SpringProject/courses_management_system/security/securityConfig.java
@@ -0,0 +1,19 @@
+package SpringProject.courses_management_system.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class securityConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+
+    }
+
+}
